28a8f5b228b034a9004fc5a0dd2e127fda71a852
lwarnb17 <war17001@byui.edu>
2017-07-13 21:58:38 -0600

    changes in final project

---
+++ finalproject/sigsagservice3.js
@@ -0,0 +1,43 @@
+function getJSON(url) {
+    return fetch(url)
+        .then(function (response) {
+        return response.json();
+    })
+        .catch(function (error) {
+        console.log(error);
+    });
+}
+function services2(){
+    getJSON("sigsagservice.json").then(function( data ){
+        console.log('test');
+        console.log(data);
+        var items= [];
+        var services2Element = document.getElementById("services");
+
+        data.services.forEach( function(val){
+            console.log(val);
+
+            var listItem = document.createElement('li');
+            var img = document.createElement('img');
+            var link = document. createElement('a');
+
+
+            link.setAttribute('href', val.url);
+            img.setAttribute('src', val.img);
+            link.innerHTML = val.link;
+
+            listItem. appendChild(img);
+            listItem. appendChild(link);
+            services2Element.appendChild(listItem);
+        });
+    })
+}
+
+
+
+
+
+
+
+
+
